5525a68d2bbb4e04b8471664f2f8f975c3f1992f
Tomohiko Hiraki <hirakitomohiko@gmail.com>
2018-06-29 18:03:53 +0200

    Create Login and Signup

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 export default class App extends React.Component {
   render() {
@@ -12,7 +14,11 @@ export default class App extends React.Component {
 
         <Appbar />
 
-        <MemoEditScreen />
+        <SignupScreen />
+
+        {/* <LoginScreen /> */}
+
+        {/* <MemoEditScreen /> */}
 
         {/* <MemoListScreen /> */}
 
